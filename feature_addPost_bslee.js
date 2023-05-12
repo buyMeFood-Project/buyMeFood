1d63674cf39f5c865a6d7c451604aff9ca13b260
bslee <s2_go@naver.com>
2023-05-12 17:44:32 +0900

    게시판등록 로컬스토리지 기초작업

--- feature_addPost_bslee.js
+++ feature_addPost_bslee.js
@@ -1,17 +1,8 @@
-function posting(){
+function feature_addPost_bslee(){
     let newPost = {
-        storeName: "새로운 가게",
-        author: "새로운 작성자",
-        star: "별점",
-        content: "내용",
-        date: new Date().toJSON().slice(0, 10),
-        rate: 5.0,
-        content: "새로운 게시글 내용입니다.",
-        imageList:["https://mblogthumb-phinf.pstatic.net/MjAxOTA3MTBfMTgz/MDAxNTYyNzU0MDU0OTky.T1dRAwsb0c8Ysj9T-lYbPGnv65UXkiSFBsikpS_x-GIg.e147DzeIngeoOuTpsAZaLOuyzkmREUSDmwNrfDdTsUUg.JPEG.newtherock/6.jpg?type=w800",
-                    "https://t1.daumcdn.net/cfile/tistory/99E39F4D5BE841E216",
-                "https://t1.daumcdn.net/cfile/tistory/185CDD584D93EED220"],
-        likeCount:0,
-        replyCount:0
+        storeName:document.getElementById("storeName").value,
+        stars:document.getElementById("stars").value,
+        content:document.getElementById("content").value,
     }
     if(localStorage.getItem('postList') == null){
         if(localStorage.getItem('postList') == null){
