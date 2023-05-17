6e71bd1745f591ebb6c9cd93b5e930a00f7b1eeb
YEJIN SEO <yejin328@naver.com>
2023-05-17 16:48:19 +0900

    join_page_1st_commit(html,js)

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,4 +1,6 @@
 function signUp() {
+    
+
     let data={
         username:document.getElementById("username").value,
         userId:document.getElementById("userid").value,
@@ -14,4 +16,46 @@ function signUp() {
         userList.push(data);
         localStorage.setItem('userList', JSON.stringify(userList));
     }
+    if(localStorage.getItem('userList').)
+};
+
+function checkIDDuplicate() {
+
+    let userList = JSON.parse(localStorage.getItem('userList'));
+
+    for (let i = 0; i < userList.length; i++) {
+        uid = userList[i].userId;
+
+        var userId = document.getElementById('userid').value;
+        if (userId === uid) {
+            $('#id_duplication').html('<a style="color:red;">중복된 ID 입니다.</a>');
+            break;
+        }
+        else {
+            $('#id_duplication').html('<a style="color:blue;">사용가능한 ID 입니다.</a>');
+        }
+    }
+    
+
+    
+};
+
+function checkNameDuplicate() {
+
+    let userList = JSON.parse(localStorage.getItem('userList'));
+
+    for (let i = 0; i < userList.length; i++) {
+        uname = userList[i].username;
+
+        var username = document.getElementById('username').value;
+        if (username === uname) {
+            $('#name_duplication').html('<a style="color:red;">중복된 닉네임 입니다.</a>');
+        }
+        else {
+            $('#name_duplication').html('<a style="color:orange;">&#10004;</a>');
+        }
+    }
+    
+
+    
 };
