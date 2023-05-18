8c15337393d5e7a32881f495c502ece42191f21a
kylesung0520 <kylesung0520@gmail.com>
2023-05-18 15:12:20 +0900

    ID/PW 찾기 코드 간소화

--- idpw/findIDPW.js
+++ idpw/findIDPW.js
@@ -2,14 +2,14 @@ const userList = JSON.parse(localStorage.getItem('userList'));
 
 // Display input form for find id info
 $("#idInfo").click(function(){
-    document.querySelector('#forID').style.display = "block";
-    document.querySelector('#forPW').style.display = "none";
+    $('#forID').css('display', '');
+    $('#forPW').css('display', 'none');
 });
 
 // Display input form for find pwd info
 $('#pwInfo').click(function(){
-    document.querySelector('#forID').style.display = "none";
-    document.querySelector('#forPW').style.display = "block";
+    $('#forID').css('display', 'none');
+    $('#forPW').css('display', '');
 });
 
 // Change option between phoneNum and email for find id
@@ -21,12 +21,23 @@ $('#byEmail').click(function(){
     $('#changeable').html("이메일: ");
 });
 
+// function to set alert message inside alert modal
+function modalControl(message){
+    event.preventDefault();
+    $('#alertContent').html(message);
+    $('#myModal').css('display', 'block');
+};
+
+// Confirm btn function to close alert modal
+$('#confirm').click(function(){
+    $('#myModal').css('display', 'none');
+});
 
 // Find Id Function
 $('#findId').click(function(){
-    const searchName = document.querySelector("#searchName").value;
-    const findOption = document.querySelector("#findOption").value;
-    let phoneNumOption = document.querySelector("#byPhoneNum");
+    const searchName = $('#searchName').val();
+    const findOption = $('#findOption').val();
+    let phoneNumOption = $('#byPhoneNum');
     if(searchName === '' || findOption === ''){
         modalControl('공란이 존재합니다. <br> 확인 후 다시 입력해주세요.');
     }
@@ -37,21 +48,19 @@ $('#findId').click(function(){
         else{
             let isExist = false;
             let retId = "";
-            for(let i = 0; i < userList.length; i++){
-                if(userList[i].username === searchName){
-                    if(phoneNumOption.checked){
-                        if(userList[i].phoneNum === findOption){
-                            isExist = true;
-                            retId = userList[i].userId;
-                            break;
-                        }
+            for(let user of userList){
+                if(phoneNumOption.prop('checked')){
+                    if(user.phoneNum === findOption){
+                        isExist = true;
+                        retId = user.userId;
+                        break;
                     }
-                    else{
-                        if(userList[i].email === findOption){
-                            isExist = true;
-                            retId = userList[i].userId;
-                            break;
-                        }
+                }
+                else{
+                    if(user.email === findOption){
+                        isExist = true;
+                        retId = user.userId;
+                        break;
                     }
                 }
             }
@@ -74,9 +83,9 @@ $('#findId').click(function(){
 
 // Find Password Function
 $('#findPW').click(function() {
-    const searchName = document.querySelector("#searchName2").value;
-    const searchID = document.querySelector("#searchID").value;
-    const searchEmail = document.querySelector("#searchEmail").value;
+    const searchName = $('#searchName2').val();
+    const searchID = $('#searchID').val();
+    const searchEmail = $('#searchEmail').val();
 
     if(searchName === '' || searchID === '' || searchEmail === ''){
         modalControl('공란이 존재합니다. <br> 확인 후 다시 입력해주세요.');
@@ -86,38 +95,26 @@ $('#findPW').click(function() {
             modalControl('일치하는 정보가 없습니다. <br> 확인 후 다시 입력해주세요.');
         }
         else{
-            let sw = 0;
-            for(let i = 0; i < userList.length; i++){
-                if(userList[i].username === searchName 
-                    && userList[i].userId === searchID
-                    && userList[i].email === searchEmail){
-                    $('#forPW').html('\
-                    <p>비밀번호 찾기 성공! <br>\
-                    임시 비밀번호를 입력하신 이메일로 발송하였습니다.\
-                    </p>\
-                    <div>\
-                        <button type="submit">메인페이지</button>\
-                        <button type="button">로그인</button>\
-                    </div>');
-                    sw = 1;
-                    break;
+            let isExist = false;
+            for(let user of userList){
+                if(user.username === searchName
+                    && user.userId === searchID
+                    && user.email === searchEmail){
+                        isExist = true;
+                        $('#forPW').html('\
+                        <p>비밀번호 찾기 성공! <br>\
+                        임시 비밀번호를 입력하신 이메일로 발송하였습니다.\
+                        </p>\
+                        <div>\
+                            <button type="submit">메인페이지</button>\
+                            <button type="button">로그인</button>\
+                        </div>');
+                        break;
                 }
             }
-            if(sw == 0){
+            if(!isExist){
                 modalControl('일치하는 정보가 없습니다. <br> 확인 후 다시 입력해주세요.');
             }
         }
     }
-});
-
-// function to set alert message inside alert modal
-function modalControl(message){
-    event.preventDefault();
-    document.querySelector('#alertContent').innerHTML = message;
-    document.querySelector('#myModal').style.display = 'block';
-}
-
-// Confirm btn function to close alert modal
-document.querySelector('#confirm').addEventListener('click', function(){
-    document.querySelector('#myModal').style.display = 'none';
 });
