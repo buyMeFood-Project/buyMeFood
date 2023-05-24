0922bff17c5e9938c5b2f0507f5193a13a891306
YEJIN325 <seoyj0325@naver.com>
2023-05-24 15:38:08 +0900

    change GNB code + findIDPW css + login, roulette style

--- login/login.js
+++ login/login.js
@@ -1,8 +1,12 @@
+import { alertModalControl } from "../alertModal/modal.js";
+$(function() {
+    $("#modalContainer").load("../alertModal/modal.html");
+});
 
 const signup = document.getElementById("sign-up");
-signin = document.getElementById("sign-in");
-loginin = document.getElementById("login-in");
-loginup = document.getElementById("login-up");
+const signin = document.getElementById("sign-in");
+const loginin = document.getElementById("login-in");
+const loginup = document.getElementById("login-up");
 
 signup.addEventListener("click", () => {
     loginin.classList.remove("block");
@@ -24,11 +28,11 @@ document.querySelector('.login__button').addEventListener("click", function(){
     var userId = document.getElementById("userId").value;
     var userPassword = document.getElementById("userPw").value;
     if (!userId) {  // 아이디 미입력
-        alert("아이디를 입력해주세요.");
+        alertModalControl("아이디를 입력해주세요.");
         
         }
     else if (!userPassword) {  // 비밀번호 미입력
-        alert("비밀번호를 입력해주세요.");
+        alertModalControl("비밀번호를 입력해주세요.");
     
     }
 
@@ -36,12 +40,12 @@ document.querySelector('.login__button').addEventListener("click", function(){
     for (var i=0; i < userList.length; i++) {
         if (userList[i].userId == userId) {
             if (userList[i].password == userPassword) {  // 로그인 성공
-                alert("로그인 성공");   
+                alertModalControl("로그인 성공");   
                 break;
             }
             else {  // 비밀번호 불일치
                 if (userPassword)
-                    alert('아이디, 비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
+                    alertModalControl('아이디, 비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
             }
         }
     }
