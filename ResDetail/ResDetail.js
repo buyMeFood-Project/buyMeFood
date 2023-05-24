43045045a026ab05312710ef9d32052604c561c8
YEJIN SEO <yejin328@naver.com>
2023-05-24 16:36:10 +0900

    GNB,Footer추가

---
+++ ResDetail/ResDetail.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#GNB").load("../gnb/gnb.html");
+  $("#footer").load("../footer/footer.html");
+});
+
+// function AddLike() {
+//   const like = document.querySelector(".heart_icon").value;
+//   like.innerText = "♥";
+// }
