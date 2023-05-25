f35100125b049bb7235378bb70bdcf89a0931449
kylesung0520 <kylesung0520@gmail.com>
2023-05-25 17:37:33 +0900

    현재 위치한 메뉴 GNB 표시, 로그인 후 User Id 표시, 비로그인 상태 게시글 추가, 좋아요, 댓글 제한 기능

--- gnb/gnb.js
+++ gnb/gnb.js
@@ -4,3 +4,25 @@ window.onload = function(){
         localStorage.setItem('search_keyword', search_keyword);
     });
 }
+$(".menu").click(function(){
+    localStorage.setItem("selectedMenu", $(this).attr('id'));
+})
+$("#toMain").click(function(){
+    $("li").removeClass("on");
+    $('#nav1').closest("li").addClass("on");
+    localStorage.setItem("selectedMenu", "nav1");
+})
+var selectedMenuItem = localStorage.getItem('selectedMenu');
+if (selectedMenuItem) {
+    $("li").removeClass("on");
+    $("#" + selectedMenuItem).closest("li").addClass("on");
+}
+
+
+var currUser = localStorage.getItem('currUser');
+
+if(currUser !== ''){
+    $('#util').hide();
+    $('#util_afterLogin').css('display', 'inline-block');
+    $('#nickName').text(" " + currUser);
+}
